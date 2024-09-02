4e5cf598300405dfb96d3c922f22d944767a145a
Muhammad Fatkurozi <fatkurozy.muhammad28@gmail.com>
2024-09-02 21:43:16 +0700

    fix: add cors

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
 import express from "express";
+import cors from "cors"
 import env from "./env.js";
 import * as handler from "./handler.js";
 
 const app = express();
+
+app.use(cors());
 
 app.get("/", handler.welcome);
 app.get("/student/pict/:nim", handler.getStudentPict);
